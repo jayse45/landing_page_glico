416e372dde95d25b8eeb4a863977c258a8e27bc2
Nana <ngyasi0@gmail.com>
2024-01-23 17:26:52 +0000

    new comming soon

--- src/pages/DesktopThree/index.jsx
+++ src/pages/DesktopThree/index.jsx
@@ -14,61 +14,62 @@ const DesktopThreePage = () => {
   return (
     <>
       <div
-        className="coming-soon bg-cover bg-no-repeat bg-white-A700 flex flex-col font-montserrat h-screen items-center justify-start mx-auto p-5 w-full sm:p-0"
+        className="coming-soon bg-cover bg-no-repeat bg-white-A700 flex flex-col font-montserrat h-screen items-center justify-start  mx-auto p-5 w-full sm:p-0"
         
       >
-        <div className="flex flex-col items-start justify-start max-w-[auto] lg-mt-[0] mx-auto md:px-5 w-full sm:h-full sm:justify-between sm:bg-gradient2">
-          <div className="flex flex-row gap-2.5 items-start justify-between max-w-[1163px] w-full">
+        <div className="flex flex-row gap-x-[60vw] items-start  max-w-[1163px] w-full sm-mt-[-330px]">
             
 
-                <Img
-                className="h-[18px] md:h-auto object-cover w-[180px]"
-                src="images/img_glicohealthcarelogo300x301.png"
-                alt="glicohealthcare"
+            <Img
+            className="h-[18px] md:h-auto object-cover w-[180px]"
+            src="images/img_glicohealthcarelogo300x301.png"
+            alt="glicohealthcare"
+            />
+
+        <div className="flex flex-col items-center justify-end w-auto">
+          <div className="flex flex-row gap-3 items-start justify-start w-auto">
+          <a href="https://web.facebook.com/GLICOHealthcare/?_rdc=1&_rdr">
+
+            <Img
+                className="h-5 w-5"
+                src="images/img_icons8facebook2_indigo_800.svg"
+                alt="icons8facebookTwo"
                 />
+          
+          </a>
 
-            <div className="flex flex-col items-center justify-start w-auto">
-              <div className="flex flex-row gap-3 items-start justify-start w-auto">
-              <a href="https://web.facebook.com/GLICOHealthcare/?_rdc=1&_rdr">
-
-                <Img
-                    className="h-5 w-5"
-                    src="images/img_icons8facebook2_indigo_800.svg"
-                    alt="icons8facebookTwo"
-                    />
-              
-              </a>
-
-              <a href="https://instagram.com/glicohealth">
-                <Img
-                    className="h-5 w-5"
-                    src="images/img_icons8instagramnew_indigo_800.svg"
-                    alt="icons8instagram"
-                    />
-              </a>
-                
-              <a href="https://twitter.com/glicohealth">
-
-                <Img
-                    className="h-5 w-5"
-                    src="images/img_icons8twitter1_indigo_800.svg"
-                    alt="icons8twitterOne"
-                    />
-              </a>
+          <a href="https://instagram.com/glicohealth">
+            <Img
+                className="h-5 w-5"
+                src="images/img_icons8instagramnew_indigo_800.svg"
+                alt="icons8instagram"
+                />
+          </a>
             
-              <a href="https://twitter.com/glicohealth">
+          <a href="https://twitter.com/glicohealth">
 
-              <Img
-                  className="h-5 w-5"
-                  src="images/img_icons8linkedin2filled_indigo_800.svg"
-                  alt="icons8linkedinTwo"
+            <Img
+                className="h-5 w-5"
+                src="images/img_icons8twitter1_indigo_800.svg"
+                alt="icons8twitterOne"
                 />
+          </a>
+        
+          <a href="https://twitter.com/glicohealth">
+
+          <Img
+              className="h-5 w-5"
+              src="images/img_icons8linkedin2filled_indigo_800.svg"
+              alt="icons8linkedinTwo"
+            />
 
 
-              </a>
-              </div>
-            </div>
+          </a>
           </div>
+        </div>
+      </div>
+        <div className="flex flex-col items-start justify-start sm:justify-end max-w-[auto] lg-mt-[0] mx-auto md:px-5 w-full sm:h-full  sm:bg-gradient2">
+          
           <div className="flex flex-col gap-5 items-start justify-center max-w-[630px] md:ml-[0] ml-[43px] mt-[10vh] w-full">
             <div className="flex flex-col gap-5 items-start justify-center w-full">
               <div className="flex flex-col gap-3 items-start justify-center w-full">
@@ -104,7 +105,7 @@ const DesktopThreePage = () => {
             Abusua apɔ mu din nti!
           </Text>
 
-          <div className="md:ml-[0] ml-[43px] mt-[4px] text-base text-blue_gray-500 ">
+          <div className="md:ml-[0] ml-[43px] mt-[24px] text-base text-blue_gray-500 ">
                 <Text
                   className="max-w-[630px] md:max-w-full sm:text-[18px] md:text-[24px] text-[32px] text-indigo-800 sm:text-white-A700 sm:font-bold"
                   size="txtMontserratBold52"
